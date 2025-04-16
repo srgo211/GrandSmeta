30cdb0be906c316659e07140e29974a17e9a837c
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-16 20:55:42 +0400

    интерфейс для загрузки и сохранения

---
+++ GrandSmeta/Services/LocalSmetXmlService.cs
@@ -0,0 +1,17 @@
+﻿using GrandSmeta.Interfaces;
+using GrandSmeta.Models;
+
+namespace GrandSmeta.Services;
+
+public class LocalSmetXmlService : ILocalSmetXmlService
+{
+    public async Task<Document?> LoadAsync(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SaveAsync(Document document, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+}
